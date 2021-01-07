482da617ad8bc31b0f38c68879e8e3683824dfe4
Ammon Lockwood <ammon@plaidtie.net>
2021-01-06 22:37:26 -0700

    feat(init): begin screeps adventures

---
+++ role.builder.js
@@ -0,0 +1,44 @@
+var roleBuilder = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+
+        if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.building = false;
+            creep.say('🔄 harvest');
+        }
+        if(!creep.memory.building && creep.store.getFreeCapacity() == 0) {
+            creep.memory.building = true;
+            creep.say('🚧 build');
+        }
+
+        if(creep.memory.building) {
+            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+            if(targets.length) {
+                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }else{
+                // nothing to build so let's repair instead
+                // TODO: probably should just make this another role 'repairer'
+                var closestDamagedStructure = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                    filter: (structure) => structure.hits < structure.hitsMax
+                });
+                if(closestDamagedStructure) {
+                    creep.repair(closestDamagedStructure);
+                }else{
+                    // no structures to repair so let's harvest instead
+                    creep.memory.role = 'harvester';
+                }
+            }
+        }
+        else {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+        }
+    }
+};
+
+module.exports = roleBuilder;
